2cc6cb14ad7faa9a77ecdefb9a932a083f9be85d
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2025-04-03 13:16:53 +0530

    add routes

--- src/index.js
+++ src/index.js
@@ -2,24 +2,52 @@ const fastify = require('fastify')({
     logger:true
 }) // root application instence 
 
-fastify.addHook('onReady', function listen(done) {
-    console.log("Server is ready to take request");
-    done()
+// fastify.addHook('onReady', function listen(done) {
+//     console.log("Server is ready to take request");
+//     done()
     
+// })
+
+// fastify.addHook('onReady', function listen() {
+//     console.log("Server is stopping");
+//    // done()
+    
+// })
+
+/* ## First method to add route  */
+
+fastify.get('/ping', () => {
+   //fastify.log.info(`incomming ping ping ping ...`);
+   return "pong"
 })
 
-fastify.addHook('onReady', function listen() {
-    console.log("Server is stopping");
-   // done()
-    
+/* ## Second  method to add route  */
+
+fastify.route({
+    url: '/hello',
+    method: "GET",
+    handler: function () {
+        console.log(fastify)
+        return "world"
+    }
 })
+
+function samplePlugin(fastify,options , done) {
+    console.log("execution my plugin");
+    fastify.decorate('key', 'value')
+    console.log(fastify);
+
+    done()
+}
+
+fastify.register(samplePlugin, {})
 
 const PORT = 8080;
 async function start() {
     try {
         await fastify.listen({port: PORT})
         console.log(`Server is up on PORT: $ ${PORT}`);
-        fastify.close()
+       // fastify.close()
     } catch (error) {
         console.log(error);
         
